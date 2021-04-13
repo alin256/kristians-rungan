5a73ce35637a3d86f385c1e19dd03fe97bce7e1e
Sergey Alyaev <cobxo3bot@gmail.com>
2021-04-13 15:49:14 +0200

    Changed mean and true setup

--- setup_run.py
+++ setup_run.py
@@ -6,33 +6,43 @@ import matplotlib.pyplot as plt
 sys.path.append('../gan-geosteering')
 from log_gan import Gan
 import csv
+
 ray.init()
 
 
-def main():
-    #np.random.seed(100)
+def main_script():
+    # np.random.seed(100)
     np.random.seed(0)
-    m_true = np.random.randn(60) * 1e-3
-    
+    numpy_input = np.load('../gan-geosteering/saves/chosen_realization_C1.npz')
+    numpy_single = numpy_input['arr_0']
+    m_true = numpy_single.copy()
+
     keys = {'bit_pos': [0, 1, 2, 3, 4, 5, 6, 7, 8],
             'vec_size': 60}
     worker = Gan.remote(keys=keys)
     task = worker.call_sim.remote(input=m_true, output_field=True)
-    img,val = ray.get(task)
-    plt.imshow(img[0,:,:]);plt.colorbar();plt.savefig('True_GAN_field.png');plt.close()
+    img, val = ray.get(task)
+    plt.imshow(img[0, :, :], interpolation='none')
+    plt.colorbar()
+    plt.savefig('True_GAN_field.png')
+    plt.close()
 
-    with open('true_data.csv','w') as f:
+    np.savez('true_img.npz', img)
+
+    with open('true_data.csv', 'w') as f:
         writer = csv.writer(f)
         for el in range(len(keys['bit_pos'])):
-            writer.writerow([str(elem) for elem in val['res'][el,:]])
+            writer.writerow([str(elem) for elem in val['res'][el, :]])
 
-    with open('data_index.csv','w') as f:
+    with open('data_index.csv', 'w') as f:
         writer = csv.writer(f)
         for el in keys['bit_pos']:
             writer.writerow([str(el)])
 
-    mean_f = np.zeros(60)
+    mean_f = m_true * 0.6
     np.savez('mean_field.npz', mean_f)
-    np.savez('true_field.npz', img)
 
-main()
+
+
+if __name__ == '__main__':
+    main_script()
